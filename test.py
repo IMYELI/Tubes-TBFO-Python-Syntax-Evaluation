376ace63f091f58b4bd419a677964ff6ba572638
imyeli <andreasindrakurniawan@gmail.com>
2021-11-24 18:33:40 +0700

    Bismillah pepeexit paling beneran

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-i = 1
-while i <= 10:
-  j = 1
-  while j <= 12:
-        print(i*j)
-        j+=1
-  i+=1
-  print("\n")
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
