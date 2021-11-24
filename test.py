e16899f3df87e9053ffe438c59048c7056c37fcf
imyeli <andreasindrakurniawan@gmail.com>
2021-11-24 22:13:37 +0700

    alhamdulilah bismillah fix banget gaada revisi lagi banget banget amin

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-
-
-def printCNF(arr):
-    level = 0
+border(30)
+if(len(lineErr)!= 0):
+    print(red_color,"Terdapat error di line")
+    for i in range(len(lineErr)):
+        print(red_color,lineErr[i],normalizer)
+else:
+    print(green_color,"Tidak terdapat kesalahan pada file python.",normalizer)
